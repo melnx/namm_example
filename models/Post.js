f74b6b5e9b82858a82bbb6a7371c993c5c6cee20
melnx <lyosha@buystand.com>
2016-08-19 01:22:41 -0700

    fix url embeds and youtube/soundcloud embeds

--- models/Post.js
+++ models/Post.js
@@ -22,12 +22,25 @@ module.exports = {
             },
         },
 
-        $init: function($scope, $http) {
+        $init: function($scope, $http, $sce) {
             $scope.$on('$routeChangeSuccess', function() {
-                $scope.$parent.controllericon = "fa-tachometer";
+                $scope.$parent.controllericon = "fa-file-text-o";
                 $scope.$parent.controllerheader = "Posts";
                 $scope.$parent.controllerdescription = "posts";
             });
+
+            $scope.getYoutubeUrl = function(url){
+                var video_id = url.split('v=')[1];
+                var ampersandPosition = video_id.indexOf('&');
+                if(ampersandPosition != -1) {
+                    video_id = video_id.substring(0, ampersandPosition);
+                }
+                return $sce.trustAsResourceUrl( 'https://www.youtube.com/embed/' + video_id );
+            }
+
+            $scope.getSoundcloudUrl = function(url){
+                return $sce.trustAsResourceUrl( 'https://w.soundcloud.com/player/?url=' + encodeURIComponent(url) + '&amp;auto_play=false&amp;hide_related=false&amp;show_comments=true&amp;show_user=true&amp;show_reposts=false&amp;visual=true' );
+            }
 
             $scope.createPost = function(){
                 if($scope.item.image && $scope.item.image.length){
--- public/client.js
+++ public/client.js
@@ -123,6 +123,22 @@
       }
     }]);
 
+    app.filter('parseUrl', function() {
+        var urls = /(\b(https?|ftp):\/\/[A-Z0-9+&@#\/%?=~_|!:,.;-]*[-A-Z0-9+&@#\/%=~_|])/gim
+        var emails = /(\w+@[a-zA-Z_]+?\.[a-zA-Z]{2,6})/gim
+
+        return function(text) {
+            if(text.match(urls)) {
+                text = text.replace(urls, "<a href=\"$1\" target=\"_blank\">$1</a>")
+            }
+            if(text.match(emails)) {
+                text = text.replace(emails, "<a href=\"mailto:$1\">$1</a>")
+            }
+
+            return text
+        }
+    });
+
     app.filter('timeago', function() {
         return function(input, p_allowFuture) {
             var substitute = function (stringOrFunction, number, strings) {
